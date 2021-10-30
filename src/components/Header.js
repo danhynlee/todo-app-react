62c43c7554736941771e117fa63462ea7f021847
danhynlee <danhynlee@gmail.com>
2021-10-29 21:20:15 -0400

    added inline styling for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,28 @@ import React from "react";
 
 // same as function Header() {}
 const Header = () => {
+  // inline styles can be passed by variables
+  const headerStyle = {
+    padding: "20px 0",
+    lineHeight: "1.5em",
+  }
   return (
-    <header>
-      <h1>todos</h1>
+    <header style={headerStyle}>
+      <h1
+        // inline styles as JSX will add JS object to attribute
+        // JSX objects within {{}} and keys are camelCase
+        style={{
+          fontSize: "6rem",
+          fontWeight: "600",
+          marginBottom: "2rem",
+          lineHeight: "1em",
+          color: "#ecece",
+          textTransform: "lowercase",
+          textAlign: "center",
+        }}
+      >
+        todos
+      </h1>
     </header>
   )
 }
